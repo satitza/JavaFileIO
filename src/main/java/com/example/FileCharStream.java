9c2ebaf265fbfaa2cb6da220c8afc6dadb51ad10
Satit Porntepanon <st_satitza@outlook.com>
2019-02-26 23:10:41 +0700

    Implement read and write using buffer class

--- src/main/java/com/example/FileCharStream.java
+++ src/main/java/com/example/FileCharStream.java
@@ -1,4 +1,48 @@
 package com.example;
 
-public class FileCharStream {
+import com.sun.tools.javac.code.Attribute;
+
+import java.io.*;
+
+public class FileCharStream implements IFileManage {
+
+    private byte[] buffers;
+
+    private CharArrayWriter caw;
+
+    private InputStreamReader read;
+    private OutputStreamWriter write;
+
+    public byte[] ReadFileStream(String fileName) {
+
+        try {
+
+            read = new InputStreamReader(new FileInputStream(fileName), "UTF-8");
+
+
+        } catch (IOException ex){
+
+            ex.printStackTrace();
+
+        }
+
+
+        return new byte[0];
+    }
+
+    public boolean WriteFileStream(String fileName, byte[] buffers) {
+        return false;
+    }
+
+
+    private BufferedReader buffer_read;
+    private BufferedWriter buffer_write;
+
+    public byte[] BufferReadFileStream(String fileName) {
+        return new byte[0];
+    }
+
+    public boolean BufferWriteFileStream(String fileName, byte[] buffers) {
+        return false;
+    }
 }
